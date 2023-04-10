c243b9e1b0d7c1dc6afc9e1abc0eaa32cbfbf4c8
damzzcode <damzzcode>
2023-04-10 15:58:33 +0700

    First Try w PHP

--- php_pertama.php
+++ php_pertama.php
@@ -1,3 +1,18 @@
 <?php
 
-echo "hallo ngab";
+    $nama_kamu = "Damzz";
+    $umur = 20;
+    //  tanda petik dua
+    
+    echo "Hello World, Welcome ".$nama_kamu;
+    echo " Umur Kamu ".$umur;
+    echo " tahun";
+    echo "<br/>";
+
+    echo "Hello World, Welcome ".$nama_kamu." Umur Kamu ".$umur." tahun";
+    echo "<br/>";
+    echo "Berarti $nama_kamu lahir di tahun ";
+    echo 2023-$umur;
+
+
+?>
