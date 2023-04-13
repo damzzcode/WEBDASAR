67aec9a198b53137481d735d1cf5424bb7ea95af
damzzcode <damzzcode>
2023-04-13 20:22:13 +0700

    New Update w Bootstrap

---
+++ try1.php
@@ -0,0 +1,45 @@
+<?php 
+    // Learn About Syntax
+
+    // Standar Output
+    // echo, print
+    // print_r 
+    // var_dump
+
+// $nama = "Adam Al Zidane";
+
+// echo "halo nama saya $nama";
+
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// $nama_depan = "Adam";
+// $nama_belakang = "Zidane";
+
+// echo $nama_depan . " ". $nama_belakang;
+
+
+// $x = 5;
+// $x += 10;
+// echo $x;
+
+// var_dump(1 > 5);
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// Identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// Operator Aritmatika
+// + - % * /
+
+// Logika
+// $x = 10;
+// var_dump($x > 30 || $x % 2 == 0);
+
+
+?>
