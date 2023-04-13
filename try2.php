df3c2fbbe247db86f8132ccaa5fe27df2a10869a
damzzcode <damzzcode>
2023-04-13 21:46:31 +0700

    Percabangan

---
+++ try2.php
@@ -0,0 +1,75 @@
+<?php 
+
+// pengulangan
+// for
+// while
+// do while
+
+// for($i = 0; $i < 5; $i++){
+//     echo "hellow world <br>";
+// }
+
+// $i = 3;
+// while ($i < 5 ){
+//     echo "yowiii <br>";
+    
+//     $i++;
+// }
+
+// $i = 0;
+// do {
+//     echo "yowiii <br>";
+// $i++;    
+
+// } while ( $i < 5);
+
+// percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+// $i = 10;
+// if($i < 15){
+//     echo "benar";
+// } else {
+//     echo "salah";
+// }
+
+$i = 20;
+if($i < 10){
+    echo "benar";
+} else if ($i == 20) {
+    echo "bingo";
+} else {
+    echo "salah";
+}
+
+?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <?php 
+        
+            for($i = 0; $i < 5; $i++){
+                echo "<tr>";
+                for ($j = 0; $j < 7; $j++){
+                    echo "<td>$i,$j</td>";
+                }
+                
+                echo "</tr>";
+            }
+        ?>
+
+</body>
+</html> -->
+
