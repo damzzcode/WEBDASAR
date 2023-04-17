ab64c15344d884e625d2021493b622cea6e5836d
damzzcode <damzzcode>
2023-04-18 02:09:35 +0700

    DATABASE MAHASISWA W SHELL

---
+++ otodidak/try3.php
@@ -0,0 +1,15 @@
+<?php 
+
+// date
+echo date("l, d-M-Y ");
+
+echo "<br> damzkuy is here ";
+
+// time
+
+$waktu = time();
+    echo "<br> Waktu saat ini: <br>".date("l, d F Y h:i:s A", $waktu);
+echo date("l", time()+60*60*24*30);
+
+
+?>
