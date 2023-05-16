d98d30e94bc4946a708276da3525606aaeb80042
damzzcode <damzzcode>
2023-05-16 10:10:56 +0700

    New

--- otodidak/try3.php
+++ otodidak/try3.php
@@ -1,15 +1,18 @@
 <?php 
 
-// date
-echo date("l, d-M-Y ");
+// // date
+// echo date("l, d-M-Y ");
 
-echo "<br> damzkuy is here ";
+// echo "<br> damzkuy is here ";
 
-// time
+// // time
 
-$waktu = time();
-    echo "<br> Waktu saat ini: <br>".date("l, d F Y h:i:s A", $waktu);
-echo date("l", time()+60*60*24*30);
+// $waktu = time();
+//     echo "<br> Waktu saat ini: <br>".date("l, d F Y h:i:s A", $waktu);
+// echo date("l", time()+60*60*24*30);
 
+for ($i = 0; $i < 10; $i++) {
+    echo "damz <br>";
+}
 
 ?>
--- php_pertama.php
+++ php_pertama.php
@@ -15,4 +15,36 @@
     echo 2023-$umur;
 
 
+for ($i = 0; $i < 10; $i++) {
+    echo "damzkuy <br>";
+}
+
+$i = 0;
+while ($i < 10;){
+    echo "damzkuy <br>";
+    $i++;
+}
+
+$i = 0;
+do {
+    echo "damzkuy <br>";
+    $i++;
+} while ($i < 10);
+
+for ($i = 0; $i < 5; $i++){
+    echo "damzet <br>";
+}
+
+$i = 0;
+while($i < 5;){
+    echo "damzet <br>";
+    $i++;
+}
+
+$i = 0;
+do{
+    echo "damzet <br>";
+    $i++;
+} while ($i < 5);
+
 ?>
